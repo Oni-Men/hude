904955a5bcc2c38badff0fd8bf78a69eca2a8f4f
Oni-Men <sensyaheis@gmail.com>
2019-09-08 14:17:38 +0900

    update test

--- test/test.js
+++ test/test.js
@@ -1,17 +1,12 @@
 const hude = Hude.mount("#hude").size(500, 500);
 
-document.body.appendChild(hude.$c);
-
 let r = 0;
 
 (function() {
 
     hude.clearWith('#fff');
 
-    hude.lineWidth(1).grid(5, 5).stroke('black');
-
-    hude.lineWidth(3);
-    hude.center().roundRect(100, 100, 10).stroke('green');
+    hude.center().circle(100).stroke('red');
 
     window.requestAnimationFrame(arguments.callee);
 })();
