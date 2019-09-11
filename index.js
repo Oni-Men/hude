9789e7bd03341058243c289e3744adaf5025d6d3
Oni-Men <senshaheis@gmail.com>
2019-09-11 22:25:31 +0900

    add many features

--- index.js
+++ index.js
@@ -1,3 +1,95 @@
+/*
+**  下記のサイトからお借りしたコードだよ
+**  https://abicky.net/2012/07/24/080446/
+**  (ちょっと改変あり)
+*/
+
+(function() {
+    CanvasRenderingContext2D.prototype._transform = [1, 0, 0, 1, 0, 0];
+    CanvasRenderingContext2D.prototype._transforms = [];
+
+    CanvasRenderingContext2D.prototype.getTransform = function() {
+        return this._transform;
+    };
+
+    CanvasRenderingContext2D.prototype.getScale = function() {
+        return {
+            x: this._transform[0],
+            y: this._transform[3]
+        };
+    };
+
+    CanvasRenderingContext2D.prototype.getTranslate = function() {
+        return {
+            x: this._transform[4],
+            y: this._transform[5]
+        };
+    };
+
+    CanvasRenderingContext2D.prototype.getRotate = function() {
+        return 0; //This must be return a value of rotation;
+    };
+
+    const restore = CanvasRenderingContext2D.prototype.restore;
+    CanvasRenderingContext2D.prototype.restore = function() {
+        this._transform = this._transforms.pop() || [1, 0, 0, 1, 0, 0];
+        restore.apply(this);
+    }
+
+    const save = CanvasRenderingContext2D.prototype.save;
+    CanvasRenderingContext2D.prototype.save = function() {
+        this._transforms.push(this._transform.slcie());
+        save.apply(this);
+    }
+
+    const rotate = CanvasRenderingContext2D.prototype.rotate;
+    CanvasRenderingContext2D.prototype.rotate = function(r) {
+        const t = [Math.cos(r), Math.sin(r), -Math.sin(r), Math.cos(r), 0, 0];
+        this._transform = multiplyTransform(this._transform, t);
+        rotate.apply(this, arguments);
+    };
+
+    const scale = CanvasRenderingContext2D.prototype.scale;
+    CanvasRenderingContext2D.prototype.scale = function(sx, sy) {
+        this._transform = multiplyTransform(this._transform, [sx, 0, 0, sy, 0, 0]);
+        scale.apply(this, arguments);
+    };
+
+    const translate = CanvasRenderingContext2D.prototype.translate;
+    CanvasRenderingContext2D.prototype.translate = function(tx, ty) {
+        this._transform = multiplyTransform(this._transform, [1, 0, 0, 1, tx, ty]);
+        translate.apply(this, arguments);
+    }
+
+    const transform = CanvasRenderingContext2D.prototype.transform;
+    CanvasRenderingContext2D.prototype.transform = function(a, b, c, d, e, f) {
+        this._transform = multiplyTransform(this._transform, arguments);
+        transform.apply(this, arguments);
+    }
+
+    const setTransform = CanvasRenderingContext2D.prototype.setTransform;
+    CanvasRenderingContext2D.prototype.setTransform = function(a, b, c, d, e, f) {
+        this._transform = Array.prototype.slice.apply(arguments);
+        setTransform.apply(this, arguments);
+    }
+
+    const multiplyTransform = function(t1, t2) {
+        return [
+            t1[0] * t2[0] + t1[2] * t2[1],
+            t1[1] * t2[0] + t1[3] * t2[1],
+            t1[0] * t2[2] + t1[2] * t2[3],
+            t1[1] * t2[2] + t1[3] * t2[3],
+            t1[0] * t2[4] + t1[2] * t2[5] + t1[4],
+            t1[1] * t2[4] + t1[3] * t2[5] + t1[5]
+        ];
+    };
+
+})();
+
+/*
+**  拝借コードここまでだよ
+*/
+
 class OriginOrder {
     static get CENTER() {
         return 'center';
@@ -20,18 +112,37 @@ class OriginOrder {
     }
 }
 
+class MouseButton {
+    static get LEFT() {
+        return 0;
+    }
+    
+    static get RIGHT() {
+        return 2;
+    }
+
+    static get MIDDLE() {
+        return 1;
+    }
+}
+
 class Hude {
     constructor(canvas) {
         this._c = canvas;
         this._g = this._c.getContext('2d');
 
         this.localOriginOrder = OriginOrder.CENTER;
+
+        this._c.addEventListener('contextmenu', e => {
+            e.preventDefault();
+        }, false);
     }
 
     static build(w, h) {
         const c = document.createElement('canvas');
-        c.width = w;
-        c.height = h;
+
+        if (w) c.width = w;
+        if (h) c.height = h;
 
         return new Hude(c);
     }
@@ -317,6 +428,13 @@ class Hude {
         this._g.lineDashOffset = v.dashOffset ? v.dashOffset : 0.0;
         this._g.lineJoin = v.join ? v.join: 'miter';
         this._g.lineWidth = v.width ? v.width :  1.0;
+        this._g.setLineDash(v.dash ? v.dash: []);
+
+        return this;
+    }
+
+    lineDash(v) {
+        this._g.setLineDash(v);
 
         return this;
     }
@@ -375,6 +493,7 @@ class Hude {
         return this;
     }
 
+    //WARNING!!! DON'T USE!!!!
     blur(iteration) {
         let i = 0;
         
@@ -391,6 +510,47 @@ class Hude {
 
         this._g.setTransform(1, 0, 0, 1, 0, 0,);
         this._g.drawImage(blured, 0, 0, blured.width, blured.height, 0, 0, this.$w, this.$h);
+
+        return this;
+    }
+    
+    _addMouseListener(type, func, mb) {
+        this.$c.addEventListener(type, e => {
+            const b = e.target.getBoundingClientRect();
+            const t = this._g.getTranslate();
+            const x = e.clientX - b.left - t.x;
+            const y = e.clientY - b.top - t.y;
+            
+            if (mb != undefined) {
+                if (mb == e.button) {
+                    func(x, y);
+                }
+            } else {
+                func(x, y);
+            }
+        });
+    }
+
+    onClick(f, mb) {
+        this._addMouseListener('click', f, mb);
+
+        return this;
+    }
+
+    onMouseUp(f, mb) {
+        this._addMouseListener('mouseup', f, mb);
+
+        return this;
+    }
+
+    onMouseDonw(f, mb) {
+        this._addMouseListener('mousedown', f, mb);
+
+        return this;
+    }
+
+    onMouseMove(f) {
+        this._addMouseListener('mousemove', f);
 
         return this;
     }
